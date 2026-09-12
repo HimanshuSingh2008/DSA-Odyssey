1b4637f458a90715251e0d19cdfdfa9576855fe6
Himanshu Singh <him_singh2008@outlook.com>
2026-09-12 13:25:04 +0530

    Implemented average calculation for array elements

--- Arrays/Cal. the avg. of elements in an array.cpp
+++ Arrays/Cal. the avg. of elements in an array.cpp
@@ -1,1 +1,30 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    int size;
+
+    cout << "Size of array: ";
+    cin >> size;
+
+    int a[size];
+
+    cout << "Enter the elements of the array: ";
+
+    for (int i = 0; i < size; i++) {
+        cin >> a[i];
+    }
+
+    int sum = 0;
+
+    for (int i = 0; i < size; i++) {
+        sum = sum + a[i];
+    }
+
+    float average = (float)sum / size;
+
+    cout << "Sum: " << sum << endl;
+    cout << "Average: " << average << endl;
+
+    return 0;
+}
